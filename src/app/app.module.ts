80df44abad88786c6a332acbfc79c3a170fbf048
Leor Seligman <seligman.leor@gmail.com>
2020-01-18 10:40:08 +0200

    components and services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ResultsComponent } from './dashboard/results/results.component';
+import { ResultsListComponent } from './dashboard/results-list/results-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ResultsComponent,
+    ResultsListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dashboard/results-list/results-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-list',
+  templateUrl: './results-list.component.html',
+  styles: []
+})
+export class ResultsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

a91e7598a00598fdb2e25555c8a713ec087a851a
Leor Seligman <seligman.leor@gmail.com>
2020-01-19 12:13:16 +0200

    observerable added

--- src/app/dashboard/results-list/results-list.component.ts
+++ src/app/dashboard/results-list/results-list.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ResultsService } from 'src/app/shared/results.service';
 
+
 @Component({
   selector: 'app-results-list',
   templateUrl: './results-list.component.html',
   styles: []
 })
 export class ResultsListComponent implements OnInit {
+  results: any = [];
 
   constructor(private service: ResultsService) { }
 
   ngOnInit() {
-    this.service.refreshList();
+this.loadResults();
   }
+
+  loadResults() {
+    return this.service.getResults().subscribe((data: {}) => {
+      this.results = data;
+    });
+  }
+
+
 
 }
--- src/app/shared/results.service.ts
+++ src/app/shared/results.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Results } from './results.model';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,27 @@ import { Results } from './results.model';
 export class ResultsService {
 readonly routeURL = 'https://localhost:5001/api/results';
   constructor(private http: HttpClient) { }
-  list: Results[];
-  refreshList() {
-    this.http.get(this.routeURL)
-    .toPromise()
-    .then(res => this.list = res as Results[]);
+ 
 
+  getResults(): Observable<Results> {
+    return this.http.get<Results>(this.routeURL)
+    .pipe(
+      retry(1),
+      catchError(this.handleError));
   }
+
+  handleError(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+ }
+
 }
+
